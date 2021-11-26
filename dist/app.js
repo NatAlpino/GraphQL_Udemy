c5e538ee557e4ba1ae158eea826f74b28a431888
Natalia Alpino <nathyalpino@hotmail.com>
2021-11-26 05:06:50 -0300

    aut gulp

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,3 @@
 console.log("Hello Word");
+
+let a = 'Hello';
--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
+//automatizando o gulp:
+
 const gulp = require('gulp');
 const clean = require('gulp-clean');
 const ts = require('gulp-typescript');
 
 const tsProject = ts.createProject('tsconfig.json');
 
-gulp.task('scripts', () => {
+/*compila o typescript e joga o javascript para o diretorio dist:
+(Static esta entre colchetes mostrando que a tarefa scripts precisa aguardar a 
+tarefa static ser executada antes)*/
+gulp.task('scripts', ['static'], () => {
     const tsResult = tsProject.src()
         .pipe(tsProject());
 
@@ -12,16 +17,37 @@ gulp.task('scripts', () => {
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('static', () => {
+/*copia os arquivos json:
+(Clean esta entre colchetes mostrando que a tarefa static precisa aguardar a 
+tarefa clean ser executada antes)*/
+gulp.task('static', ['clean'], () => {
     return gulp
         .src(['src/**/*.json'])
         .pipe(gulp.dest('dist'));
 });
 
+//limpar o diretorio:
 gulp.task('clean', () => {
     return gulp
         .src('dist')
         .pipe(clean());  
 });
 
-gulp.task('build', ['clean', 'static', 'scripts']);
+/*ordem das tarefas que serão executadas:
+Nao precisei colocar todas as tarefas em ordem, porque em cada parametro eu ja 
+tenho qual tarefa cada uma deve esperar antes de ser executada*/
+gulp.task('build', ['scripts']);
+
+
+//automatizando o gulp para ele ter acesso a cada alteraçao:
+gulp.task('watch', ['build'], () => {
+    return gulp.watch(['src/**/*.ts', 'src/**/*.json'], ['build'],)
+//dentro dos colchetes temos os arquivos que o build deve ficar atento para alteraçoes
+});
+
+//automatizando a ordem das tarefas do gulp:
+gulp.task('default', ['watch']);
+
+
+
+
